b8ade95e47e1035fd5a02c2419a97ae442fe004c
StarterCode11 <infokumarpratyush03@gmail.com>
2023-04-22 23:00:02 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+
+
+const App = () => {
+  const [temperatureValue, setTemperatureValue] = useState(10);
+  const [temperatureColor, setTemperatureColor] = useState("cold");
+
+  const increaseTemperature = () => {
+    if(temperatureValue === 30) return;
+
+    const newTemperature = temperatureValue+1;
+
+    setTemperatureValue(newTemperature);
+    if(newTemperature >= 15){
+      setTemperatureColor("hot");
+    }
+  };
+
+  const DecreaseTemperature = () => {
+    if(temperatureValue === 0) return;
+
+    const newTemperature = temperatureValue - 1;
+
+    setTemperatureValue(newTemperature);
+    if(newTemperature < 20){
+      setTemperatureColor("cold");
+    }
+  };
+
+  return (
+    <div className="app-container">
+      <div className="temperature-display-container">
+      <div className={`temperature-display ${temperatureColor}`}>
+          {temperatureValue}°C
+        </div>
+        </div>
+      <div className="button-container">
+        <button onMouseDown={increaseTemperature}>+</button>
+        <button onClick={DecreaseTemperature}>-</button>
+      </div>
+      
+    </div>
+  );
+};
+
+export default App;
